b21b16cb592528d16c12f00ee1e332c5ff612a72
Mamun <mamun511510782@gmail.com>
2021-01-18 20:12:12 +0600

    Create all essential model, controller, migration

---
+++ app/Http/Controllers/TinyCategoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tiny_category;
+use Illuminate\Http\Request;
+
+class TinyCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Tiny_category  $tiny_category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tiny_category $tiny_category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Tiny_category  $tiny_category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tiny_category $tiny_category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Tiny_category  $tiny_category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tiny_category $tiny_category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Tiny_category  $tiny_category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tiny_category $tiny_category)
+    {
+        //
+    }
+}
---
+++ app/Models/Tiny_category.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tiny_category extends Model
+{
+    use HasFactory;
+}
